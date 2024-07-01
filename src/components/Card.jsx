4cb0eea3cc092dc15d8bdfdf47f62d87ccb374ea
Karan <email@karanshergill.com>
2024-07-01 12:40:06 +0530

    fixed styled components

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,38 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const CardContainer = styled.div.attrs({
+    className: 'flex flex-col shadow-md rounded-b-lg overflow-hidden hover:scale-105 transition-transform duration-200 cursor-pointer hover:shadow-lg'
+})``;
+
+const CardImage = styled.img.attrs({
+    className: 'w-full h-40 object-cover rounded-t-lg border-b-2'
+})``;
+
+const CardContent = styled.div.attrs({
+    className: 'flex flex-col gap-1 p-6 mt-2 mb-4 h-44'
+})``;
+
+const CardTitle = styled.h2.attrs({
+    className: 'text-lg font-bold mb-2'
+})``;
+
+const CardTextField = styled.p.attrs({
+    className: 'text-sm font-medium'
+})``;
 
 export const Card = ({ name, population, region, capital, flag }) => {
     return (
         <a href="#">
-            <div className='flex flex-col shadow-md rounded-b-lg overflow-hidden hover:scale-105 transition-transform duration-200 cursor-pointer hover:shadow-lg'>
-                <img className='w-full h-40 object-cover rounded-t-lg border-b-2' src={flag} alt={`${name} Flag`} />
-                <div className='flex flex-col gap-1 p-6 mt-2 mb-4 h-44'>
-                    <h2 className='text-lg font-bold mb-2'>{name}</h2>
-                    <p className='text-sm font-medium'><span className='text-sm font-bold'>Population: </span>{population.toLocaleString('en-IN')}</p>
-                    <p className='text-sm font-medium'><span className='text-sm font-bold'>Region: </span>{region}</p>
-                    <p className='text-sm font-medium'><span className='text-sm font-bold'>Capital: </span>{capital || 'N/A'}</p>
-                </div>
-            </div>
+            <CardContainer>
+                <CardImage src={ flag } alt={`${name} Flag`} />
+                <CardContent>
+                    <CardTitle>{name}</CardTitle>
+                    <CardTextField><span className='text-sm font-bold'>Population: </span>{population.toLocaleString('en-IN')}</CardTextField>
+                    <CardTextField><span className='text-sm font-bold'>Region: </span>{region}</CardTextField>
+                    <CardTextField><span className='text-sm font-bold'>Capital: </span>{capital || 'N/A'}</CardTextField>
+                </CardContent>
+            </CardContainer>
         </a>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 import './App.css'
 import './index.css'
+
+// routing setup
+const browserRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: '/country',
+    element: <App />
+  },
+])
 
 // modifications made to the default boiler plate code
 const root = createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={browserRouter} />
   </React.StrictMode>,
 )
