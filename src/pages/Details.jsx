7f8d7490836405a4d1ebedf622f8a5ed4ac9d138
Karan <email@karanshergill.com>
2024-07-03 16:21:19 +0530

    updated rendering logic

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -22,18 +22,65 @@ export const Details = () => {
     const fetchCountryDetails = async () => {
       try {
         const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`)
-        const data = await response.json()
+        if (!response.ok) {
+          throw new Error('Fetching data from API failed!');
+        }
+        const data = await response.json();
+        if (data.length > 0) {
+          const country = data[0];
+          console.log((country));
+
+          setCountryDetails({
+            name: country.name.common,
+            nativeName: Object.values(country.name.nativeName).map(native => native.common).join(', '),
+            population: country.population,
+            region: country?.region,
+            subregion: country?.subregion,
+            capital: country?.capital,
+            topLevelDomain: country?.tld,
+            currencies: Object.values(country.currencies).map(currency => currency.name).join(', '),
+            languages: Object.values(country.languages).map(language => language).join(', '),
+            borders: Object.values(country.borders).map(border => border).join(', ')
+          });
+        }
+        else {
+          throw new Error('Data not found!');
+        }
       }
       catch (error) {
+        setError(error.message);
         console.log("Error: Fetching data from API failed!")
       }
-    }
+    };
+    fetchCountryDetails();
   }, [countryName])
 
+  // conditional rendering
+
+  if (countryDetails === null && !error) {
+    return (
+      <div>Loading...</div>
+    )
+  };
+
+  if (error) {
+    return (
+      <div>{error}</div>
+    )
+  }
 
   return (
     <div>
-      Country
+      Name: {countryDetails?.name}
+      Native Name: {countryDetails?.nativeName}
+      Population: {countryDetails?.population.toLocaleString('en-IN')}
+      Region: {countryDetails?.region}
+      Sub Region: {countryDetails?.subregion}
+      Capital: {countryDetails?.capital}
+      Top Level Domain: {countryDetails?.topLevelDomain}
+      Currencies: {countryDetails?.currencies}
+      Languages: {countryDetails?.languages}
+      Border Countries: {countryDetails?.borders}
     </div>
   )
 }
